044d6496d5bce837c31a57cf1cc5c43aa9d11048
Aman308 <116431870+Aman308@users.noreply.github.com>
2024-09-26 12:45:29 +0530

    second commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+
+import PropTypes from 'prop-types'
+
 export class News extends Component {
- 
+  
+  static defaultProps = {
+    country : 'us',
+    pageSize : 6,
+    category : 'general'
+  }
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
   constructor(){
     super();
     this.state ={
@@ -13,7 +27,7 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=76b6aa7972734674afc86cf87c618c6f&page=1&pageSize=${this.props.pageSize}`
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=76b6aa7972734674afc86cf87c618c6f&page=1&pageSize=${this.props.pageSize}`
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -22,7 +36,7 @@ export class News extends Component {
   }
 
 handlePrevClick = async() =>{
-  let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=76b6aa7972734674afc86cf87c618c6f&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+  let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=76b6aa7972734674afc86cf87c618c6f&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
  this.setState({loading: true})
   let data = await fetch(url);
   let parsedData = await data.json();
@@ -38,7 +52,7 @@ handlePrevClick = async() =>{
     
     if(!( this.state.page + 1 > Math.ceil(this.state.totalResults
       /this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=76b6aa7972734674afc86cf87c618c6f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=76b6aa7972734674afc86cf87c618c6f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true})
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -62,7 +76,7 @@ handlePrevClick = async() =>{
         <div className="row mx-2 bg-dark text-light">
         {!this.state.loading && this.state.articles.map((element)=>{
           return <div className="col md-4 my-3 mx-3 bg-dark text-light"key={element.url}> 
-           <NewsItem  title={element.title?element.title.slice(0, 45):""} description={element.description ? element.description.slice(0, 80):""} imageUrl = {element.urlToImage} newsUrl = {element.url}/>
+           <NewsItem  title={element.title?element.title.slice(0, 45):""} description={element.description ? element.description.slice(0, 80):""} imageUrl = {element.urlToImage} newsUrl = {element.url} author={element.author} date={element.publishedAt} source = {element.source.name}/>
            </div>
         })}
        
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
- let {title , description, imageUrl ,newsUrl} = this.props
+ let {title , description, imageUrl ,newsUrl,author,date , source} = this.props
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-  <img src={!imageUrl ? "https://cdn.pixabay.com/photo/2017/06/26/19/03/news-2444778_640.jpg":imageUrl} className="card-img-top" alt="..."/>
+        <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '85%' , zIndex: '1'}}>{source}</span>
+  <img src={!imageUrl ? "https://cdn.pixabay.com/photo/2017/06/26/19/03/news-2444778_640.jpg":imageUrl}  className="card-img-top" alt="..."/>
   <div className="card-body bg-dark text-light border border-light">
-    <h5 className="card-title">{title}... </h5>
+    <h5 className="card-title">{title}...  </h5>
     <p className="card-text">{!description ? "Read the full news by clicking on read more below": description}...</p>
+    <p className="card-text bg-primary rounded-2 py-1 px-2"><small className="text-body-white ">By {author? author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
     <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
@@ -19,3 +21,4 @@ export class NewsItem extends Component {
 }
 
 export default NewsItem
+
